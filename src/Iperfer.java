a95a09cd6a1a208f80b7299bfdd2d1da95706bb3
Xiaoyu.Liu <liuxy199803@163.com>
2022-02-12 15:04:06 -0600

    edited-Iperfer-client

--- src/Iperfer.java
+++ src/Iperfer.java
@@ -1,43 +1,69 @@
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.Arrays;
+import java.lang.Integer;
+import java.lang.String;
+import java.util.Date;
 
 public class Iperfer {
     public static void main(String[] args){
         byte[] bytes = new byte[1000];
         Arrays.fill( bytes, (byte) 0 );
-        if (args[0] == "-c"){
+        if (args[0] == "-c") {
             //client mode
-            if (args.length == 7){
-            String hostname = args[2];
-            Int port = args[4];
-            // port 1024 to 65535
-            if (port < 1024 || port > 65535){
-                System.out.println("Error: port number must be in the range 1024 to 65535");
-                System.exit(0);
-            }
-            Int time = args[6];
-            }
-            else {
+            String hostname = null;
+            Integer port_num = null;
+            Integer time = null;
+            if (args.length == 7) {
+                hostname = args[2];
+                port_num = Integer.valueOf(args[4]);
+                // port 1024 to 65535
+                if (port_num < 1024 || port_num > 65535) {
+                    System.out.println("Error: port number must be in the range 1024 to 65535");
+                    System.exit(0);
+                }
+                time = Integer.valueOf(args[6]);
+            } else {
                 System.out.println("Error: wrong type of mode");
                 System.exit(0);
             }
             //connect
-            Socket client = new Socket(hostname, port);
-            CountingOutputStream out = new CountingOutputStream(socket.getOutputStream());
+            Socket client = null;
+            try {
+                client = new Socket(hostname, port_num);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            DataOutputStream out = null;
+            try {
+                out = new DataOutputStream(client.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             long start = System.currentTimeMillis();
             long elapsed = 0L;
-            while (elapsed < time * 1000){
-                out.write(bytes);
+            long sent_bytes = 0L;
+            while (elapsed < time * 1000) {
+                try {
+                    out.write(bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 //                out.flush();
+                sent_bytes = sent_bytes + 1;
                 elapsed = (new Date()).getTime() - start;
             }
-            out.close();
-            long sent_bytes = out.getByteCount();
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         else if(args[0] == "-s"){
             //server mode
             if (args.length == 3){
-                Int port = args[2];
+                Integer port = Integer.valueOf(args[2]);
                 // port 1024 to 65535
                 if (port < 1024 || port > 65535){
                     System.out.println("Error: port number must be in the range 1024 to 65535");
