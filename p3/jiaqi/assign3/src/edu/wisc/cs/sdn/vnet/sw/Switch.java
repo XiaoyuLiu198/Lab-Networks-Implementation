58b53fd449c67eda4911e7f031fd0420ac88c55a
redffish <he279@wisc.edu>
2022-04-13 00:52:58 -0500

    add files

--- p3/jiaqi/assign3/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ p3/jiaqi/assign3/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,10 +1,6 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import net.floodlightcontroller.packet.Ethernet;
-import net.floodlightcontroller.packet.MACAddress;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
@@ -12,56 +8,50 @@ import edu.wisc.cs.sdn.vnet.Iface;
 /**
  * @author Aaron Gember-Jacobson
  */
-public class Switch extends Device {
-	class TableEntry {
-		Iface iface;
-		long time;
-
-		TableEntry(Iface iface, long time) {
-			this.iface = iface;
-			this.time = time;
-		}
-	}
-
-	Map<MACAddress, TableEntry> table = new HashMap<>();
-
+public class Switch extends Device
+{
+	private MACTable macTable;
+	
 	/**
 	 * Creates a router for a specific host.
-	 * 
-	 * @param host
-	 *            hostname for the router
+	 * @param host hostname for the router
 	 */
-	public Switch(String host, DumpFile logfile) {
-		super(host, logfile);
+	public Switch(String host, DumpFile logfile)
+	{
+		super(host,logfile);
+		this.macTable = new MACTable();
 	}
 
 	/**
 	 * Handle an Ethernet packet received on a specific interface.
-	 * 
-	 * @param etherPacket
-	 *            the Ethernet packet that was received
-	 * @param inIface
-	 *            the interface on which the packet was received
+	 * @param etherPacket the Ethernet packet that was received
+	 * @param inIface the interface on which the packet was received
 	 */
-	public void handlePacket(Ethernet etherPacket, Iface inIface) {
-		System.out.println("*** -> Received packet: "
-				+ etherPacket.toString().replace("\n", "\n\t"));
-
-		table.put(etherPacket.getSourceMAC(),
-				new TableEntry(inIface, System.currentTimeMillis()));
-
-		TableEntry entry = table.get(etherPacket.getDestinationMAC());
-		boolean isBroadcast = entry == null
-				|| (System.currentTimeMillis() - entry.time > 15000);
-
-		if (isBroadcast) {
-			for (Iface intf : interfaces.values()) {
-				if (!intf.equals(inIface)) {
-					sendPacket(etherPacket, intf);
+	public void handlePacket(Ethernet etherPacket, Iface inIface)
+	{
+		System.out.println("*** -> Received packet: " +
+                etherPacket.toString().replace("\n", "\n\t"));
+		
+		/********************************************************************/
+		/* TODO: Handle packets                                             */
+		
+		this.macTable.insert(etherPacket.getSourceMAC(), inIface);
+		
+		MACTableEntry entry = this.macTable.lookup(etherPacket.getDestinationMAC());
+		if (entry != null)
+		{ this.sendPacket(etherPacket, entry.getInterface()); }
+		else
+		{
+			for (Iface iface : this.interfaces.values()) 
+			{
+				if (iface != inIface)
+				{
+					this.sendPacket(etherPacket, iface);
+					System.out.println("Send packet out interface "+iface);
 				}
 			}
-		} else {
-			sendPacket(etherPacket, entry.iface);
 		}
+		
+		/********************************************************************/
 	}
 }
