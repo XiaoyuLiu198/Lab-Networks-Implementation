56b1148d6a65540f9bb60d4db0f24225e627249e
Xiaoyu.Liu <liuxy199803@163.com>
2022-04-16 00:57:04 -0500

    test90

--- p3/xiaoyu/assign3/src/edu/wisc/cs/sdn/vnet/Main.java
+++ p3/xiaoyu/assign3/src/edu/wisc/cs/sdn/vnet/Main.java
@@ -88,7 +88,8 @@ public class Main
 			// Read static route table
 			if (routeTableFile != null)
 			{ ((Router)dev).loadRouteTable(routeTableFile); }
-			else { ((Router)dev).rip(); }
+			else { }
+				// ((Router)dev).rip(); }
 			
 			// Read static ACP cache
 			if (arpCacheFile != null)
