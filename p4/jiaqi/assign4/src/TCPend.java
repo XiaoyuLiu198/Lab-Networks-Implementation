f9e1434adb2c447959952ce8c350d96e3b41f71f
redffish <he279@wisc.edu>
2022-04-27 14:27:35 -0500

    test

--- p4/jiaqi/assign4/src/TCPend.java
+++ p4/jiaqi/assign4/src/TCPend.java
@@ -1,48 +1,58 @@
-import java.util.*;
-
-class TCPend {
-
+public class TCPend {
+    private static final short DEFAULT_PORT = 8888;
     public static void main(String[] args) {
-        if(args.length != 12 && args.length != 6) {
+        short port = DEFAULT_PORT;        // port number at which the client will run
+        String remoteIP = null;           // the IP address of receiver
+        short remotePort = DEFAULT_PORT;  // the port at which the remote receiver is running
+        String fileName = null;           // the file to be sent
+        int mtu = -1;                     // maximum transmission unit in bytes
+        int sws = -1;                     // sliding window size in number of segments
+        
+        if (args.length == 12) {
+            for (int i = 0; i < args.length; i++) {
+                String arg = args[i];
+                if (arg.equals("-p")) {
+                    port = Short.parseShort(args[++i]);
+                } else if (arg.equals("-s")) {
+                    remoteIP = args[++i];
+                } else if (arg.equals("-a")) {
+                    remotePort = Short.parseShort(args[++i]);
+                } else if (arg.equals("-f")) {
+                    fileName = args[++i];
+                } else if (arg.equals("-m")) {
+                    mtu = Integer.parseInt(args[++i]);
+                } else if (arg.equals("-c")) {
+                    sws = Integer.parseInt(args[++i]);
+                } else {
+                    System.out.println("Wrong command.");
+                    return;
+                }
+            }
+            
+            Sender sender = new Sender(port, remoteIP, remotePort, fileName, mtu, sws);
+
+        } else if (args.length == 6) {
+            for (int i = 0; i < args.length; i++) {
+                String arg = args[i];
+                if (arg.equals("-p")) {
+                    port = Short.parseShort(args[++i]);
+                } else if (arg.equals("-m")) {
+                    mtu = Integer.parseInt(args[++i]);
+                } else if (arg.equals("-c")) {
+                    sws = Integer.parseInt(args[++i]);
+                } else if (arg.equals("-f")) {
+                    fileName = args[++i];
+                } else {
+                    System.out.println("Wrong command.");
+                    return;
+                }
+            }
+
+            Receiver receiver = new Receiver(port, mtu, sws);
+
+        } else {
+            System.out.println("Wrong command.");
             return;
         }
-        int port = 0;
-        String remoteIP = null;
-        int serverport = 0;
-        String filename = null;
-        int mtu = 0;
-        int sws = 0;
-        if(args.length == 12) {
-            if(args[0].equals("-p") && args[2].equals("-s") && args[4].equals("-a") && args[6].equals("-f") && args[8].equals("-m") && args[10].equals("-c")) {
-                // Get respective params
-                port = Integer.parseInt(args[1]);
-                remoteIP = args[3];
-                serverport = Integer.parseInt(args[5]);
-                filename = args[7];
-                mtu = Integer.parseInt(args[9]);
-                sws = Integer.parseInt(args[11]);
-
-                // Construct the entity Host
-                Client curClient = new Client(port, remoteIP, serverport, filename, mtu, sws);
-                curClient.connectionRequest();
-                curClient.runClient();
-            } else {
-                System.out.println("Usage for client: java TCPend -p <port> -s <remote-IP> -a <remote-port> -f <file name> -m <mtu> -c <sws>");
-            } 
-        } else if(args.length == 6) {
-            if(args[0].equals("-p") && args[2].equals("-m") && args[4].equals("-c")) {
-                // Get respective params
-                port = Integer.parseInt(args[1]);
-                mtu = Integer.parseInt(args[3]);
-                sws = Integer.parseInt(args[5]);
-
-                // Construct the entity Client
-                Host curHost = new Host(port, mtu, sws);
-                curHost.runHost();
-            } else {
-                System.out.println("Usage for server: java TCPend -p <port> -m <mtu> -c <sws>");
-            }
-        }
-
     }
 }
